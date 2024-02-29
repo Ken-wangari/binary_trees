0a6e0109fa4a3258c49e1f3ff8c363ca6ac62cc1
Ken-wangari <wangariken50@gmail.com>
2024-02-29 01:12:50 -0800

    2-binary_tree_insert_right.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,21 +2,24 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+    if (parent == NULL)
+        return (NULL);
 
-	if (parent == NULL)
-		return (NULL);
+    // Create a new node
+    binary_tree_t *new = binary_tree_node(parent, value);
+    if (new == NULL)
+        return (NULL);
 
-	new = binary_tree_node(parent, value);
-	if (new == NULL)
-		return (NULL);
+    // If parent already has a right child, update pointers
+    if (parent->right != NULL)
+    {
+        new->right = parent->right;
+        parent->right->parent = new;
+    }
 
-	if (parent->right != NULL)
-	{
-		new->right = parent->right;
-		parent->right->parent = new;
-	}
-	parent->right = new;
+    // Set the new node as the right child of the parent
+    parent->right = new;
 
-	return (new);
+    return (new);
 }
+
