c2bc93df6690965016be869e87e1967a0a7187c2
Ken-wangari <wangariken50@gmail.com>
2024-03-01 15:41:15 -0800

    132-array_to_heap.c

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -1,39 +1,21 @@
 #include "binary_trees.h"
 
 /**
- * array_to_heap_recursive - Recursively builds a Max Binary Heap tree from an array.
- * @root: Pointer to the root node of the heap.
- * @array: Pointer to the array.
- * @index: Index of the current element in the array.
- * @size: Size of the array.
- *
- * Return: A pointer to the root node of the created Binary Heap.
+ * array_to_heap - builds a Max Binary Heap tree from an array
+ * @array: a pointer to the first element of the array to be converted
+ * @size: the number of element in the array
  */
-heap_t *array_to_heap_recursive(heap_t *root, int *array, size_t index, size_t size)
-{
-    if (index < size)
-    {
-        root = heap_insert(&root, array[index]);
-        array_to_heap_recursive(root, array, 2 * index + 1, size);
-        array_to_heap_recursive(root, array, 2 * index + 2, size);
-    }
-    return root;
-}
 
-/**
- * array_to_heap - Builds a Max Binary Heap tree from an array.
- * @array: Pointer to the first element of the array to be converted.
- * @size: The number of elements in the array.
- *
- * Return: A pointer to the root node of the created Binary Heap.
- */
 heap_t *array_to_heap(int *array, size_t size)
 {
+    unsigned int i;
     heap_t *root = NULL;
 
-    if (array && size > 0)
-        root = array_to_heap_recursive(root, array, 0, size);
+    if (array == NULL || size == 0)
+        return (NULL);
 
-    return root;
+    for (i = 0; i < size; i++)
+        heap_insert(&root, array[i]);
+
+    return (root);
 }
-
